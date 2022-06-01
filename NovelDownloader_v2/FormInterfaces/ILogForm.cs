a4ec88c9d9cb318a27575a0cd440ab390237dab9
gmastergreatee <g77471164@gmail.com>
2022-06-01 21:30:44 +0530

    ..4

---
+++ NovelDownloader_v2/FormInterfaces/ILogForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovelDownloader_v2.FormInterfaces
+{
+    public interface ILogForm : IForm
+    {
+        void AppendText(string text);
+    }
+}
