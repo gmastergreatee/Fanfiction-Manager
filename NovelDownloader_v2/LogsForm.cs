980d0e630df5933d419c8b688b3d39d8701f0796
gmastergreatee <g77471164@gmail.com>
2022-06-02 21:31:10 +0530

    ..5

---
+++ NovelDownloader_v2/LogsForm.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NovelDownloader_v2.FormInterfaces;
+
+namespace NovelDownloader_v2
+{
+    public partial class LogsForm : Form, ILogForm
+    {
+        #region EventHandlers
+        public event EventHandler OnCloseClick;
+        #endregion
+
+        public LogsForm()
+        {
+            InitializeComponent();
+            txtConsole.ContextMenu = new ContextMenu(new MenuItem[]
+            {
+                new MenuItem("Select All", (sender, e) =>
+                {
+                    txtConsole.SelectAll();
+                }),
+                new MenuItem("Copy", (sender, e) =>
+                {
+                    Clipboard.SetText(txtConsole.SelectedText);
+                }),
+                new MenuItem("Clear", (sender, e) =>
+                {
+                    txtConsole.Clear();
+                }),
+            });
+        }
+
+        private void Logs_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            OnCloseClick?.Invoke(sender, e);
+            e.Cancel = true;
+            Hide();
+        }
+
+        public void AppendText(string text)
+        {
+            txtConsole.AppendText(text + Environment.NewLine);
+        }
+    }
+}
