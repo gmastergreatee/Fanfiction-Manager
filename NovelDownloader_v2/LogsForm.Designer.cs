dc8e69b194e234fded685dccedc8aaf79f63dbd8
gmastergreatee <g77471164@gmail.com>
2022-06-03 21:31:35 +0530

    ..6

---
+++ NovelDownloader_v2/LogsForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace NovelDownloader_v2
+{
+    partial class LogsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtConsole = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtConsole
+            // 
+            this.txtConsole.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtConsole.Location = new System.Drawing.Point(0, 0);
+            this.txtConsole.Multiline = true;
+            this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
+            this.txtConsole.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtConsole.Size = new System.Drawing.Size(932, 441);
+            this.txtConsole.TabIndex = 0;
+            this.txtConsole.WordWrap = false;
+            // 
+            // LogsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(932, 441);
+            this.Controls.Add(this.txtConsole);
+            this.MinimizeBox = false;
+            this.Name = "LogsForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logs";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Logs_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtConsole;
+    }
+}
